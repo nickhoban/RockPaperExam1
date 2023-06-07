64d5549f7eb5b2a9739242b663f5b5ae53ea6303
nickhoban <110017566+nickhoban@users.noreply.github.com>
2023-06-07 13:56:44 +0800

    Added html, css and js

---
+++ script.js
@@ -0,0 +1,20 @@
+<script>
+
+//Create a function for myMessage
+
+    function say(myMessage){
+          var msg = new SpeechSynthesis(myMessage);
+          msg.text = "Hello World";
+          window.speechSynthesis.speak(msg);
+        }
+
+        function play(user){
+            let result ="";
+
+            if (user ==='rock'){
+                result = tie
+            }
+        }
+
+
+</script>

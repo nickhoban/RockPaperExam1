55d229f12873084dbcd835b90244533b8903b211
nickhoban <110017566+nickhoban@users.noreply.github.com>
2023-06-07 13:58:13 +0800

    Added JS

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 <script>
+      
+function say(myMessage){
+  var msg = new SpeechSynthesis(myMessage);
+  msg.text = "Hello World";
+  window.speechSynthesis.speak(msg);
+}
 
-//Create a function for myMessage
+/* This is a simplified version of the game.
+The Computer ALWAYS picks ROCK.  
+Does the user's choose win,lose, or tie vs ROCK? */
 
-    function say(myMessage){
-          var msg = new SpeechSynthesis(myMessage);
-          msg.text = "Hello World";
-          window.speechSynthesis.speak(msg);
-        }
-
-        function play(user){
-            let result ="";
-
-            if (user ==='rock'){
-                result = tie
-            }
-        }
-
-
+function play(user) {
+    let result = "";
+    if (user === 'rock') {
+        result = "tie";
+    }
+    if (user === 'paper') {
+        result = "win";
+    }
+    if (user === 'scissors') {
+        result = "lose";
+    }
+    var theMessage = ("Computer chose rock, you " + result + "!");
+    alert(theMessage);
+    say(theMessage);
+}
 </script>
